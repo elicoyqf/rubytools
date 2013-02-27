54bee6f4375b480f7758a97f029de19c91f264a0
elico <elico_yqf@sina.com>
2013-02-27 20:58:11 +0800

    更新了新的格式。

--- csv_test.rb
+++ csv_test.rb
@@ -3,7 +3,7 @@ require 'csv'
 
 #通过添加指定编码进行转换成utf-8进行读取数据。
 i = 0
-CSV.foreach('lib/file/HTTP_201302262200.csv',encoding: "GB2312:UTF-8",headers: true) do |row|
+CSV.foreach('lib/file/HTTP_201302271700.csv',encoding: 'GB2312:UTF-8',headers: true) do |row|
   puts row
   #puts '-'*80+'>>'
   i += 1
@@ -11,7 +11,7 @@ end
 puts "i is -------->#{i}"
 
 #其实是二维数据直接进行读取也可行。
-data = CSV.read('lib/file/HTTP_201302262200.csv',encoding: 'GB2312:UTF-8')
+data = CSV.read('lib/file/HTTP_201302271700.csv',encoding: 'GB2312:UTF-8')
 puts data[1][1]
 
 #CSV.open('lib/file/HTTP_201302262200.csv',"r:Unicode:UTF-8").each do | line |
